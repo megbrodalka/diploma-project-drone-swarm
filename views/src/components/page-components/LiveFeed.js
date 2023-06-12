ed2393c780e4eeb93803d7890024d0597267a898
megbrodalka <megbrodalka@hotmail.com>
2023-06-12 15:25:31 +0800

    designed video streaming page

--- views/src/components/page-components/LiveFeed.js
+++ views/src/components/page-components/LiveFeed.js
@@ -1,11 +1,10 @@
-
 import React from 'react'
 import VideoStream from "../small-components/VideoStream";
 
 const LiveFeed = () => {
     return (
-        <div className="flex-col justify-center items-center">
-            <h1>Live Feed</h1>
+        <div className="m-8 lg:w-3/5 mx-auto px-10">
+            <h1 className="font-semibold text-xl text-gray-600 dark:text-white mb-4">Live Feed</h1>
             <VideoStream/>
         </div>
     );
--- views/src/components/small-components/VideoStream.js
+++ views/src/components/small-components/VideoStream.js
@@ -1,46 +1,54 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 
+const port = "http://localhost:8000/api"
+
 const VideoStream = () => {
     const [streaming, setStreaming] = useState(false);
 
-  const startStreaming = async () => {
-    try {
-      await axios.get('http://localhost:8000/api/start_stream');
-      setStreaming(true);
-    } catch (error) {
-      console.error('Error starting streaming:', error);
-    }
-  };
+    const startStreaming = async () => {
+      try {
+        await axios.get(`${port}/start_stream`);
+        setStreaming(true);
+      } catch (error) {
+        console.error('Error starting streaming:', error);
+      }
+    };
 
-  const stopStreaming = async () => {
-    try {
-      await axios.get('http://localhost:8000/api/stop_stream');
-      setStreaming(false);
-    } catch (error) {
-      console.error('Error stopping streaming:', error);
-    }
-  };
+    const stopStreaming = async () => {
+      try {
+        await axios.get(`${port}/stop_stream`);
+        setStreaming(false);
+      } catch (error) {
+        console.error('Error stopping streaming:', error);
+      }
+    };
 
-  return (
-    <div>
+    const buttonStyle = "w-1/3 bg-blue-700 text-white p-1 mt-2 rounded-lg hover:bg-blue-800" +
+        " dark:bg-blue-600 dark:hover:bg-blue-700";
+
+    return (
       <div>
-        {streaming ? (
-          <button onClick={stopStreaming}>Stop Streaming</button>
-        ) : (
-          <button onClick={startStreaming}>Start Streaming</button>
-        )}
+        <div className="border border-gray-300 w-full h-[500px] rounded-lg dark:border-gray-600">
+          {streaming ? (
+            <img className="object-cover w-full h-full" src="http://localhost:8000/api/live_feed" alt="Video" />
+          ) : (
+            <div className="flex justify-center items-center h-full">
+              <h1 className="text-center text-gray-600 dark:text-white">Waiting for stream to start...</h1>
+            </div>
+          )}
+        </div>
+
+          <div className="flex justify-center mt-4">
+              {streaming ? (
+                <button className={buttonStyle} onClick={stopStreaming}>Stop Streaming</button>
+              ) : (
+                <button className={buttonStyle} onClick={startStreaming}>Start Streaming</button>
+              )}
+        </div>
+
       </div>
-      <div>
-        {streaming && (
-          <img
-            src="http://localhost:8000/api/live_feed"
-            alt="Video"
-          />
-        )}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default VideoStream
